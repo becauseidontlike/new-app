09a1c99d91c892a3670510234d393ab1027ba603
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-23 13:41:19 +0100

    axios changes

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,28 @@
-import React from "react";
-import "./Weather.css";
+import React, { useState } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
+import "./Weather.css";
 
 export default function Weather(){
+const [weather, setWeather] = useState({ready: false});
+
+function handleResponse(response) {
+console.log(response.data);
+setWeather({
+    ready: true,
+    temperature: response.data.main.temp,
+    wind: response.data.wind.speed,
+    humidity: response.data.main.humidity,
+    city: response.data.name,
+    icon: response.data.weather[0].icon,
+    description: response.data.weather[0].description,
+    date: "MONDAY",
+});
+}
+
+    
+if (weather.ready) {
     return (
     <div className="Weather">
         <div className="container">
@@ -13,32 +32,38 @@ export default function Weather(){
                 <input type="search" placeholder="Search for a city..." className="form-control" autoFocus="on" />
                 </div> 
                 <div className="col-2">
-                <input type="submit" value="Search" className="btn btn-primary w-100" />
+                <input type="submit" value="Search" className="btn btn-outline-secondary w-100" />
                 </div>
                 <div className="col-2">
-                <input type="submit" value="Current" className="btn btn-primary w-100" />
+                <input type="submit" value="Current" className="btn btn-outline-secondary  w-100" />
                 </div>
              </div>
             </form>
-            <h1>Kraków</h1>
+            <h1>{weather.city}</h1>
             <ul>
-                <li>Monday 11:40</li>
-                <li>Mostly cloudly</li>
+                <li>{weather.date}</li>
+                <li className="text-capitalize">{weather.description}</li>
             </ul>
             <div className="row mt-4">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="weather-icon"></img>
-               <span className="temperature">-5</span><span className="unit">℃</span>
+                    <img src="http://openweathermap.org/img/wn/${weather.icon}@2x.png" alt={weather.description}></img>
+               <span className="temperature">{Math.round(weather.temperature)}</span><span className="unit">℃</span>
                 </div>
                 <div className="col-6">
                     <ul>
-                        <li>Precipitation: 15%</li>
-                        <li>Humidity: 30%</li>
-                        <li>Wind: 3 m/s</li>
+                        <li>Humidity: {weather.humidity}%</li>
+                        <li>Wind: {weather.wind} m/s</li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
-    )
+    );
+} else {
+    let city = "Krakow";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6c8161756616103589832909859e4f86&units=metric`
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..."
+}
 }

4576de8fad4ecce40fc4d365d61c218510c94e37
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-23 11:36:49 +0100

    new weather app created from scratch with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">  
+      <p>My weather app</p>
+      <Weather />
+      </div>
+        <div className="Footer">
+            <footer className="sign">Coded with ☕&🍪 by WP<br /><a href="https://github.com/becauseidontlike/react-weather"
+             target="_blank" rel="noreferrer">Open-source code</a></footer>
+        </div>
     </div>
   );
 }

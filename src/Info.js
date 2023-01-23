4aabd9a21b4f05237f7cf7bc70d74a389fdb022f
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-23 20:18:36 +0100

    before T changes

--- src/Info.js
+++ src/Info.js
@@ -4,23 +4,28 @@ import DateFormat from "./DateFormat";
 export default function Info(props){
     return(
     <div className="Info">
-           <h1>{props.dataInfo.city}</h1>
+        <h1>{props.dataInfo.city}</h1>
+        <div className="row">
+           <div className="col-6">
             <ul>
                 <li><DateFormat date={props.dataInfo.date} /></li>
                 <li className="text-capitalize">{props.dataInfo.description}</li>
             </ul>
-            <div className="row mt-4">
-                <div className="col-6">
-                    <img src={props.dataInfo.icon} alt={props.dataInfo.description}></img>
-               <span className="temperature">{Math.round(props.dataInfo.temperature)}</span><span className="unit">℃</span>
-                </div>
-                <div className="col-6">
+            </div>
+            <div className="col-6">
                     <ul>
                         <li>Humidity: {props.dataInfo.humidity}%</li>
                         <li>Wind: {props.dataInfo.wind} m/s</li>
                     </ul>
-                </div>
             </div>
-     </div>
+       </div>
+            <div className="row mt-4">
+                <div className="col-6">
+                    <img src={props.dataInfo.icon} alt={props.dataInfo.description}></img>
+                    <span className="temperature">{Math.round(props.dataInfo.temperature)}</span><span className="unit">℃</span>
+                </div>
+            
+            </div>
+    </div>
     );
 }
